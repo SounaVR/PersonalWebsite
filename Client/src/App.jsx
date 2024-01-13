38a515e4780bd89fd99c13e48cf026fa3d625ef1
Souna <59102661+SounaVR@users.noreply.github.com>
2024-01-13 15:51:00 +0100

    routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Welcome to Our Landing Page</h1>
-            </header>
-        </div>
-    )
-}
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/projects" element={<Projects />}></Route>
+                <Route path="/contact" element={<Contact />}></Route>
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
---
+++ Client/src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className="Projects">
+            <header className="App-header">
+                <h1>Welcome to Our Projects Page</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Projects;
