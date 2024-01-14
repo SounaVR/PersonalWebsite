3547c645e4217b48ccb236865f8439ce173da21f
Souna <59102661+SounaVR@users.noreply.github.com>
2024-01-14 10:07:11 +0100

    changed nodemailer service

--- Server/src/server.js
+++ Server/src/server.js
@@ -18,11 +18,10 @@ app.use(express.static(path.join(__dirname, '../../Client/dist')));
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
-    host: 'sandbox.smtp.mailtrap.io',
-    port: 2525,
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.GMAIL_APP_PASSWORD
     }
 });
 
