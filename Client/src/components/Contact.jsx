7558942515f571272ad2dc4176a21cf13c22248d
Souna <59102661+SounaVR@users.noreply.github.com>
2024-01-16 16:13:08 +0100

    https send mail fix

--- Client/src/components/Contact.jsx
+++ Client/src/components/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch('http://localhost:5000/api/send-email', {
+            const response = await fetch('https://localhost:5000/api/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
