ef9832e5fce45095ab024a15d0e719b7721dbc9c
Souna <59102661+SounaVR@users.noreply.github.com>
2024-01-19 13:11:04 +0100

    Projects cards

--- Client/src/components/Projects.jsx
+++ Client/src/components/Projects.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
+
+import '../css/Projects.css';
+
+const projectsList = [
+    {
+        title: 'Project 1',
+        description: 'Description for Project 1.',
+        techStack: ['HTML5', 'CSS3', 'JavaScript'],
+        imageUrl: 'https://via.placeholder.com/300x200'
+    },
+    {
+        title: 'Project 2',
+        description: 'Description for Project 2.',
+        techStack: ['React', 'NodeJS', 'MongoDB'],
+        imageUrl: 'https://via.placeholder.com/300x200'
+    },
+];
 
 const Projects = () => {
     return (
-        <div className="Projects">
-            <header className="App-header">
-                <h1>Welcome to Our Projects Page</h1>
-            </header>
-        </div>
-    )
-}
+       <div className="cards-root">
+            {projectsList.map((project, index) => (
+                <ProjectCard key={index} {...project} />
+            ))}
+       </div>
+    );
+};
 
 export default Projects;
